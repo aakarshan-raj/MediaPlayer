07f62848692eadb5f986baf8ebc8d3e1f2bec55f
aakarshan-raj <aakarshan7raj@gmail.com>
2023-03-16 13:06:16 +0530

    first commit

---
+++ app/src/main/java/com/example/video_player/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.video_player
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.VideoView
+import android.widget.MediaController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val uri:Uri= Uri.parse("android.resource://"+packageName+"/"+"test")
+ 
+    }
+}
